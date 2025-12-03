2f81b8e7cea6b192d49171e3aebc8430630d5d90
Feleknazsnl <240541039@firat.edu.tr>
2025-12-03 21:06:06 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -3,21 +3,28 @@ public class Exercise4_2 {
   // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
 
     public static void main(String[] args) {
-        zippo("rattle", 13);
+        zippo("rattle", 13);//1
     }
 
     public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
+        System.out.println(blimp);//5
+        zippo("ping", -5);//6
     }
 
     public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
+        if (flag < 0) {//7
+            System.out.println(quince + " zoop");//8
         } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+            System.out.println("ik");//2
+            baffle(quince);//3  // quince = "rattle"
+            System.out.println("boo-wa-ha-ha");//9
         }
     }
 }
+/*program ciktisi
+ik
+rattle
+ping zoop
+boo-wa-ha-ha*/
+
+//tüm satırlar sadece bir kere çalıştı
